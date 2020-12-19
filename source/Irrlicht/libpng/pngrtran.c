51598db3bb7851fe5ea1ad7658b3106027251211
cutealien <cutealien@dfc29bdd-3216-0410-991c-e03cc46cb475>
2020-12-19 15:03:11 +0000

    Merging r6145 through r6171 from trunk to ogl-es branch
    git-svn-id: svn://svn.code.sf.net/p/irrlicht/code/branches/ogl-es@6172 dfc29bdd-3216-0410-991c-e03cc46cb475

--- source/Irrlicht/libpng/pngrtran.c
+++ source/Irrlicht/libpng/pngrtran.c
@@ -21,7 +21,7 @@
 // iOS Undefined symbols for architecture arm64
 /*#ifdef PNG_ARM_NEON_IMPLEMENTATION
 #  if PNG_ARM_NEON_IMPLEMENTATION == 1
-#    define PNG_ARM_NEON_INTRINSICS_AVAILABLE 
+#    define PNG_ARM_NEON_INTRINSICS_AVAILABLE
 #    if defined(_MSC_VER) && defined(_M_ARM64)
 #      include <arm64_neon.h>
 #    else
