bd9321796a3795aacf39018bbee2a9aa2f50f6c3
Maksim <Maksym48@pm.me>
2022-08-22 23:58:59 +0300

    Direct3D O_o

--- source/Irrlicht/CIrrDeviceSDL2.cpp
+++ source/Irrlicht/CIrrDeviceSDL2.cpp
@@ -34,10 +34,6 @@ namespace irr
 {
 	namespace video
 	{
-		#ifdef _IRR_COMPILE_WITH_DIRECT3D_9_
-		IVideoDriver* createDirectX9Driver(const irr::SIrrlichtCreationParameters& params,
-			io::IFileSystem* io, HWND window);
-		#endif
 
 		#ifdef _IRR_COMPILE_WITH_OPENGL_
 		IVideoDriver* createOpenGLDriver(const SIrrlichtCreationParameters& params,
@@ -449,32 +445,10 @@ void CIrrDeviceSDL2::createDriver()
 	switch(CreationParams.DriverType)
 	{
 	case video::DEPRECATED_EDT_DIRECT3D8_NO_LONGER_EXISTS:
-		os::Printer::log("DIRECT3D8 Driver is no longer supported in Irrlicht. Try another one.", ELL_ERROR);
-		break;
-
 	case video::EDT_DIRECT3D9:
-		#ifdef _IRR_COMPILE_WITH_DIRECT3D_9_
-		os::Printer::log("SDL2 device does not support DIRECT3D9 driver. Try another one.", ELL_ERROR);
-		#else
-		os::Printer::log("DIRECT3D9 Driver was not compiled into this dll. Try another one.", ELL_ERROR);
-		#endif // _IRR_COMPILE_WITH_DIRECT3D_9_
-
-		break;
-
 	case video::EDT_SOFTWARE:
-		#ifdef _IRR_COMPILE_WITH_SOFTWARE_
-		os::Printer::log("SDL2 device does not support SOFTWARE driver. Try another one.", ELL_ERROR);
-		#else
-		os::Printer::log("No Software driver support compiled in.", ELL_ERROR);
-		#endif
-		break;
-
 	case video::EDT_BURNINGSVIDEO:
-		#ifdef _IRR_COMPILE_WITH_BURNINGSVIDEO_
-		os::Printer::log("SDL2 device does not support BURNINGSVIDEO driver. Try another one.", ELL_ERROR);
-		#else
-		os::Printer::log("Burning's video driver was not compiled in.", ELL_ERROR);
-		#endif
+		os::Printer::log("SDL2 device does not support this driver. Try another one.", ELL_ERROR);
 		break;
 
 	case video::EDT_OPENGL:
