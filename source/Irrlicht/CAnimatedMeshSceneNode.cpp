ae974399daeea09cee755d41511257b9e71017e4
luk3yx <luk3yx@users.noreply.github.com>
2026-07-03 23:55:23 +1200

    Call getBoundingBox() after getMesh() (#41)

--- source/Irrlicht/CAnimatedMeshSceneNode.cpp
+++ source/Irrlicht/CAnimatedMeshSceneNode.cpp
@@ -830,9 +830,9 @@ void CAnimatedMeshSceneNode::setMesh(IAnimatedMesh* mesh)
 	}
 
 	// get materials and bounding box
+	IMesh* m = Mesh->getMesh(0,0);
 	Box = Mesh->getBoundingBox();
 
-	IMesh* m = Mesh->getMesh(0,0);
 	if (m)
 	{
 		Materials.clear();
