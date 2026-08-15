39bff2c5232790105d8064c158461b78dd40053d
Maksym H. <Maksym48@pm.me>
2026-08-15 16:26:23 +0200

    Update minizip-ng to 4.2.2

--- source/Irrlicht/minizip-ng/mz_os.h
+++ source/Irrlicht/minizip-ng/mz_os.h
@@ -11,10 +11,6 @@
 #ifndef MZ_OS_H
 #define MZ_OS_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 /***************************************************************************/
 
 #if defined(__APPLE__)
@@ -27,7 +23,7 @@ extern "C" {
 #  define MZ_VERSION_MADEBY_HOST_SYSTEM (MZ_HOST_SYSTEM_UNIX)
 #endif
 
-#if defined(HAVE_LZMA) || defined(HAVE_LIBCOMP)
+#if defined(HAVE_LZMA) || defined(HAVE_LIBCOMP) || defined(HAVE_PPMD)
 #  define MZ_VERSION_MADEBY_ZIP_VERSION (63)
 #elif defined(HAVE_WZAES)
 #  define MZ_VERSION_MADEBY_ZIP_VERSION (51)
@@ -49,13 +45,26 @@ extern "C" {
 
 /***************************************************************************/
 
-#if defined(_WIN32)
+#include "mz_config.h"
+
+#if HAVE_DIRENT_H
+#  include <dirent.h>
+#elif HAVE_SYS_DIRENT_H
+#  include <sys/dirent.h>
+#else
 struct dirent {
     char d_name[256];
 };
-typedef void *DIR;
-#else
-#  include <dirent.h>
+#endif
+
+#if !HAVE_PDIR
+typedef struct DIR DIR;
+#endif
+
+/***************************************************************************/
+
+#ifdef __cplusplus
+extern "C" {
 #endif
 
 /***************************************************************************/
@@ -90,6 +99,12 @@ int32_t mz_path_remove_extension(char *path);
 
 int32_t mz_path_get_filename(const char *path, const char **filename);
 /* Get the filename from a path */
+
+int32_t mz_path_is_symlink_target_safe(const char *link_path, const char *target, const char *base_path);
+/* Checks if a symlink target resolves within base path. */
+
+int32_t mz_dir_has_unsafe_symlink(const char *path, const char *base_path);
+/* Checks if any existing component of path is a symlink that escapes base path. */
 
 int32_t mz_dir_make(const char *path);
 /* Creates a directory recursively */
@@ -134,10 +149,16 @@ int32_t mz_os_set_file_date(const char *path, time_t modified_date, time_t acces
 /* Sets a file's modified, access, and creation dates if supported */
 
 int32_t mz_os_get_file_attribs(const char *path, uint32_t *attributes);
-/* Gets a file's attributes */
+/* Gets a file's attributes, following symbolic links */
 
 int32_t mz_os_set_file_attribs(const char *path, uint32_t attributes);
 /* Sets a file's attributes */
+
+int32_t mz_os_get_temp_path(char *path, int32_t max_path, const char *prefix);
+/* Gets a unique temporary file path */
+
+int32_t mz_os_path_same_fs(const char *path_a, const char *path_b);
+/* Checks if both paths are on the same filesystem */
 
 int32_t mz_os_make_dir(const char *path);
 /* Recursively creates a directory */
@@ -150,11 +171,17 @@ struct dirent *mz_os_read_dir(DIR *dir);
 int32_t mz_os_close_dir(DIR *dir);
 /* Closes a directory that has been opened for listing */
 
+int32_t mz_os_is_dir_separator(char c);
+/* Checks to see if character is a directory separator */
+
 int32_t mz_os_is_dir(const char *path);
 /* Checks to see if path is a directory */
 
 int32_t mz_os_is_symlink(const char *path);
 /* Checks to see if path is a symbolic link */
+
+int32_t mz_os_get_link_attribs(const char *path, uint32_t *attributes);
+/* Gets a symbolic link's attributes */
 
 int32_t mz_os_make_symlink(const char *path, const char *target_path);
 /* Creates a symbolic link pointing to a target */
